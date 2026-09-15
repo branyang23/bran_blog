3ce4dfd01a35961dd81e563f682639b32d4b1fd7
branyang <branyang@outlook.com>
2026-09-15 20:23:47 +0800

    turn on cloudflare's image transformation

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,7 @@ export default defineConfig({
 			subsets: ['latin'],
 		},
 	],
+	image: {
+		service: { entrypoint: './src/image-service.ts' },
+	},
 });
---
+++ src/image-service.ts
@@ -0,0 +1,36 @@
+import type { ExternalImageService } from 'astro';
+
+const service: ExternalImageService = {
+	getURL(options) {
+		const params = new URLSearchParams();
+
+		if (options.width) params.set('width', String(options.width));
+		if (options.height) params.set('height', String(options.height));
+		if (options.quality) params.set('quality', String(options.quality));
+		params.set('format', options.format ?? 'auto');
+		params.set('fit', 'scale-down');
+		params.set('onerror', 'redirect');
+
+		const opts = params.toString().replace(/&/g, ',');
+		const src = typeof options.src === 'string' ? options.src : options.src.src;
+		const source = src.startsWith('/') ? src.slice(1) : src;
+
+		return `/cdn-cgi/image/${opts}/${source}`;
+	},
+
+	getHTMLAttributes(options) {
+		const { src, format, quality, ...attributes } = options;
+
+		return {
+			...attributes,
+			loading: options.loading ?? 'lazy',
+			decoding: options.decoding ?? 'async',
+		};
+	},
+
+	validateOptions(options) {
+		return options;
+	},
+};
+
+export default service;
